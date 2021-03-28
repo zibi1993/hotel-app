17a89c40b0f99b6f27abe6f7474e2b239d456b7f
zibi <zibi@zibis-MacBook-Pro.local>
2021-03-29 00:32:09 +0200

    Create components

---
+++ src/components/Hotels/Hotel/Hotel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Hotel() {
+  return (
+    <div>
+      Hotel
+    </div>
+  )
+}
+export default Hotel;
---
+++ src/components/Hotels/Hotels.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Hotel from './Hotel/Hotel';
+
+class Hotels extends Component{
+render(){
+  return(
+    <div>
+      <Hotel/>
+    </div>
+  )
+}
+
+}
+export default Hotels;
